3c9274ae7e3baa996a1f5211b7a221bd3ca72694
Nguyễn Phú Khương <khuongnguyen07092005@gmail.com>
2021-07-29 17:04:14 +0700

    PHP server get access_token facebook push hosting

---
+++ server-api-get-token-facebook.php
@@ -0,0 +1,36 @@
+<?php
+
+
+//use in python requests.get('http://<hostname>/server-api-get-token-facebook.php?cookie=<cookie>
+
+
+session_start();
+$cookie=htmlentities($_REQUEST['cookie']);
+$ch=curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://m.facebook.com/composer/ocelot/async_loader/?publisher=feed');
+$head[] = "Connection: keep-alive";
+$head[] = "Keep-Alive: 300";
+$head[] = "authority: m.facebook.com";
+$head[] = "ccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
+$head[] = "accept-language: vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5";
+$head[] = "cache-control: max-age=0";
+$head[] = "upgrade-insecure-requests: 1";
+$head[] = "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+$head[] = "sec-fetch-site: none";
+$head[] = "sec-fetch-mode: navigate";
+$head[] = "sec-fetch-user: ?1";
+$head[] = "sec-fetch-dest: document";
+curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+curl_setopt($ch, CURLOPT_ENCODING, '');
+curl_setopt($ch, CURLOPT_COOKIE, $cookie);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $head);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+$access = curl_exec($ch);
+curl_close($ch);
+echo(explode('\",\"useLocalFilePreview',explode('accessToken\":\"', $access)[1])[0]);
+?>
