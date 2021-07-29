c36c562fbf91fb0519de54661db64ef7ede3f225
Nguyễn Phú Khương <khuongnguyen07092005@gmail.com>
2021-07-29 10:59:36 +0700

    class record
    ghi dữ liệu giống kiểu bản ghi bằng class

---
+++ class-record.py
@@ -0,0 +1,11 @@
+class __main__:pass #create Class
+
+Record=__main__() #create an empty employee record
+
+Record.name=' NPK =)))' #fill the data of the record
+Record.age=20
+Record.subject='math'
+
+print(Record.name) #print data record
+print(Record.age)
+print(Record.subject)
