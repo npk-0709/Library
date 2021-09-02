8f822ee68cb3b432d532dadd0f741f30aa0dfe39
Nguyễn Phú Khương <khuongnguyen07092005@gmail.com>
2021-09-02 11:26:45 +0700

    Delete time__.py

--- time__.py
+++
@@ -1,8 +0,0 @@
-import time
-def get_time():
-    hour   = time.strftime("%H", time.localtime())
-    minute = time.strftime("%M", time.localtime())
-    second = time.strftime("%S", time.localtime())
-    return[hour,minute,second]
-
-print(get_time())
