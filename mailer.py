293c2161d214bcdf8495f38506f6d6971e69f097
Nguyễn Phú Khương <khuongnguyen07092005@gmail.com>
2021-10-21 22:12:27 +0700

    Update mailer.py
    send mail  by python

--- mailer.py
+++ mailer.py
@@ -1,17 +1,23 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-fromaddr = "YOUR MAIL"
-toaddr = "SEND TO "
-msg = MIMEMultipart()
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "(SUBJECT)"
-body = "BODY"
-msg.attach(MIMEText(body, 'plain'))
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(fromaddr, "YOUR PASSWORD")
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
-server.quit()
+#
+class MAILER:
+    def __init__(self,email_bot:str,password_bot:str,to_email:str,title:str,msg:str) -> None:
+        self.BOT_EMAIL = email_bot
+        self.BOT_PASSWORD = password_bot
+        self.TO_EMAIL = to_email
+        self.TITLE = title
+        self.MSG = msg
+    def send(self):
+        msg = MIMEMultipart()
+        msg['From'] = self.BOT_EMAIL
+        msg['To'] = self.TO_EMAIL
+        msg['Subject'] = self.TITLE
+        msg.attach(MIMEText(self.MSG, 'plain'))
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(self.BOT_EMAIL, self.BOT_PASSWORD)
+        text = msg.as_string()
+        server.sendmail(self.BOT_PASSWORD, self.TO_EMAIL, text)
+        server.quit()
